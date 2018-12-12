e34fa28e7197604e86903c200686bff0902dfd4d
Reed Oei <oei.reed@gmail.com>
2018-12-12 12:14:10 -0600

    Added code that actually tests something for the purpose of testing selection.

---
+++ src/main/java/com/reedoei/testrunner/example/java/Config.java
@@ -0,0 +1,32 @@
+package com.reedoei.testrunner.example.java;
+
+public class Config {
+    private static int SETTING_A = 0;
+    private static int SETTING_B = 0;
+
+    public static void settingA(final int settingA) {
+        SETTING_A = settingA;
+    }
+
+    public static int settingA() {
+        return SETTING_A;
+    }
+
+    public static void settingB(final int settingB) {
+        SETTING_B = settingB;
+    }
+
+    public static int settingB() {
+        return SETTING_B;
+    }
+
+    public static void setBothSettings(final int settingA, final int settingB) {
+        SETTING_A = settingA;
+        SETTING_B = settingB;
+    }
+
+    public static void reset() {
+        settingA(0);
+        settingB(0);
+    }
+}
---
+++ src/test/java/com/reedoei/testrunner/example/java/ConfigTest.java
@@ -0,0 +1,34 @@
+package com.reedoei.testrunner.example.java;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.*;
+
+// use this to ensure that dependency does not manifest in the default order
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ConfigTest {
+    @Test
+    public void aTestSetPolluter() {
+        Config.settingA(10);
+        assertEquals(10, Config.settingA());
+        Config.settingB(5);
+        assertEquals(5, Config.settingB());
+    }
+
+    @Test
+    public void bTestResetCleaner() {
+        Config.settingA(92);
+        Config.settingB(5);
+        Config.reset();
+        assertEquals(0, Config.settingA());
+        assertEquals(0, Config.settingB());
+    }
+
+    @Test
+    public void cTestDefaultSettingsVictim() {
+        assertEquals(0, Config.settingA());
+        assertEquals(0, Config.settingB());
+    }
+}
